efe1fbce89de12d0a13a659bb5a409e50d1c9573
Patrick Schleizer <adrelanos@riseup.net>
2014-12-14 21:24:23 +0000

    new file: usr/share/tb-updater/tb_internal_updater_disable.js

---
+++ usr/share/tb-updater/tb_internal_updater_disable.js
@@ -0,0 +1,11 @@
+// This file is part of Whonix
+// Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+// See the file COPYING for copying conditions.
+
+// Deactivating Tor Browser’s Internal Updater at least as long it does not
+// support verification. See also:
+// - https://www.whonix.org/blog/tor-browser-updater-warning
+// - https://www.whonix.org/forum/index.php/topic,807
+// Feel free to comment this out or to delete this file if you know what you
+// are doing.
+pref("app.update.enabled", false);
