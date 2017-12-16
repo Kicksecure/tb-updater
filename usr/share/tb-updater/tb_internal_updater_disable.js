5250c8ff791e6c871d03d5d59578e4d57773acd3
Patrick Schleizer <adrelanos@riseup.net>
2017-12-16 21:46:44 +0000

    remove non-ASCII character

--- usr/share/tb-updater/tb_internal_updater_disable.js
+++ usr/share/tb-updater/tb_internal_updater_disable.js
@@ -2,7 +2,7 @@
 // Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
 // See the file COPYING for copying conditions.
 
-// Deactivating Tor Browser’s Internal Updater at least as long it does not
+// Deactivating Tor Browser's Internal Updater at least as long it does not
 // support verification. See also:
 // - https://www.whonix.org/blog/tor-browser-updater-warning
 // - https://www.whonix.org/forum/index.php/topic,807
