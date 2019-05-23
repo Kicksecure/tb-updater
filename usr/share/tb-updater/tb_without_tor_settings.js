c047a66b4fe61ceee63e4ab72e097e8997e3fa78
Patrick Schleizer <adrelanos@riseup.net>
2019-05-23 06:43:20 -0400

    coypright

--- usr/share/tb-updater/tb_without_tor_settings.js
+++ usr/share/tb-updater/tb_without_tor_settings.js
@@ -1,5 +1,5 @@
 // This file is part of Whonix
-// Copyright (C) 2012 - 2018 ENCRYPTED SUPPORT LP <adrelanos@riseup.net>
+// Copyright (C) 2012 - 2019 ENCRYPTED SUPPORT LP <adrelanos@riseup.net>
 // See the file COPYING for copying conditions.
 
 // Warning! These settings disable Tor. You will not be anonymous!
